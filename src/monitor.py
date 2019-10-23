72f3f2d067d181b653d9618b7607eda94b2c7b71
unknown <luke.garceau@kavokerr.com>
2019-10-23 14:26:43 -0400

    added the funcitonality for running a command

--- src/monitor.py
+++ src/monitor.py
@@ -1,5 +1,7 @@
 import os, sys, subprocess
 
 # funciton to list runnig processes
-def listProcesses():
-    
+def getMsStatus():
+    command = ['systemctl', 'status', 'msIot', '-l']
+    output = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True).communicate()[0]
+    print(output)    
