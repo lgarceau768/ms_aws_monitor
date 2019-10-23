3891d21f21aaee600eb536cd254f6e944e0d1e44
unknown <luke.garceau@kavokerr.com>
2019-10-23 15:29:22 -0400

    ready for the first test

---
+++ dist/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+deviceName=$(hostname)
+cd /home/User1/
+git clone https://lgarceau768:Spook524*@github.com/lgarceau768/ms_aws_monitor.git
+tr -d '\15\32' < /home/User1/ms_aws_monitor/src/runService.sh > /home/User1/ms_aws_monitor/src/runService.sh
+mv /home/User1/ms_aws_monitor/src/monitor.service /etc/systemd/system/
+systemctl enable monitor
+systemctl start monitor
+sleep 20
+systemctl status monitor -l > /home/User1/out/$deviceName_monitor_setup.log
---
+++ src/runService.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/User1/ms_aws/monitor/src
+python3 monitor.py
