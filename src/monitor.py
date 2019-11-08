eacf2474cc7eb29c3bb6e3ed823ce350636f3b87
Luke Garceau <luke.garceau@kavokerr.com>
2019-11-08 12:09:22 -0500

    working on the iptables portion

--- src/monitor.py
+++ src/monitor.py
@@ -1,8 +1,24 @@
 import os, sys, subprocess
 
-p = subprocess.Popen(['systemctl', 'is-active', 'msIot'], stdout=subprocess.PIPE)
-(output, err) = p.communicate()
-output = output.decode('utf-8')
+hostname = 'iotc-edbc3300-b4bb-4461-8328-e2d8bf17d7a9.azure-devices.net'
 
-print(output)
+# returns True/False based on if the service is running
+def getStatus(process):
+    p = subprocess.Popen(['systemctl', 'is-active', process], stdout=subprocess.PIPE)
+    (output, err) = p.communicate()
+    output = output.decode('utf-8')
+    if 'in-active' in output:
+        return False
+    else:
+        return True
+
+def nslookup(ip):
+    p = subprocess.Popen(['nslookup', ip], stdout=subprocess.PIPE)
+    (output, err) = p.communicate()
+    print(output)
+
+
+### Main Loop
+nslookup(hostname)
+
 
