926de32f6d0e1a8660aa6912c5f9669b6cfe9a94
unknown <luke.garceau@kavokerr.com>
2019-11-11 10:53:34 -0500

    commit

--- dist/install.sh
+++ dist/install.sh
@@ -11,7 +11,7 @@ git clone https://lgarceau768:Spook524*@github.com/lgarceau768/ms_aws_monitor.gi
 
 # install the tool
 tr -d '\15\32' < /home/User1/ms_aws_monitor/src/runService.sh > /home/User1/ms_aws_monitor/src/runService.sh
-mv /home/User1/ms_aws_monitor/src/monitor.service /etc/systemd/system/
+mv /home/User1/ms_aws_monitor/dist/monitor.service /etc/systemd/system/
 
 # install dependencies
 sudo apt-get install dnsutils -y
