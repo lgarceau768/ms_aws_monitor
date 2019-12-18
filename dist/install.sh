277ff086507d7b17509d0d239d766a6fc1331d8f
Luke Garceau <luke.garceau@kavokerr.com>
2019-12-18 10:07:46 -0500

    added functionality for units without the ms platform

--- dist/install.sh
+++ dist/install.sh
@@ -5,9 +5,16 @@
 # record devicename
 deviceName=$(hostname)
 
+# install dependencies
+sudo apt-get install git -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install dnsutils -y
+
 # download the tool
 cd /home/User1/
 git clone https://lgarceau768:Spook524*@github.com/lgarceau768/ms_aws_monitor.git
+
 
 # install the tool
 echo '#!/bin/bash' > /home/User1/ms_aws_monitor/src/runService.sh
@@ -16,8 +23,8 @@ echo 'python3 monitor.py' >> /home/User1/ms_aws_monitor/src/runService.sh
 mv /home/User1/ms_aws_monitor/dist/monitor.service /etc/systemd/system/
 
 # install dependencies
-sudo apt-get install dnsutils -y
 python3 -m pip install psutil
+python3 -m pip install getmac
 
 # enable / start the service
 systemctl enable monitor
